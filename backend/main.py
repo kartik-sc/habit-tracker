1652bb4596ab3e1ee739284d6e071a119f5b47fa
kartik-sc <kartiksatishchandra@gmail.com>
2026-04-03 12:25:35 +0530

    update main.py with CORS

--- backend/main.py
+++ backend/main.py
@@ -6,8 +6,13 @@ import models.habit
 
 Base.metadata.create_all(bind=engine)
 app=FastAPI(title="Discipline Dashboard",version="1.0.0")
-app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:5173"],allow_methods = ["*"],allow_headers=["*"])
 app.include_router(habits_router)
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/health")
 def health(): return {"status": "ok"}
